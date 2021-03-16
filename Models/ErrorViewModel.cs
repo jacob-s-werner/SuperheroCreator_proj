c65c68629b298a4c6f49090e4a1db215b2dfefb9
Jacob Werner <jacob.scott.werner@gmail.com>
2021-03-16 12:50:54 -0500

    Initial Commit of skeleton project. Created Superhero Model that has Name, AlterEgo, PrimaryAbility, SecondaryAbility, and Catchphrase.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SuperheroCreator_proj.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Superhero.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperheroCreator_proj.Models
+{
+    public class Superhero
+    {
+
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string AlterEgo { get; set; }
+        public string PrimaryAbility{ get; set; }
+        public string SecondaryAbility { get; set; }
+        public string Catchphrase { get; set; }
+
+
+    }
+}
