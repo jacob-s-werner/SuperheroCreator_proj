0eb561723eeac639d0ad880be0366a18bb2ccf15
Jacob Werner <jacob.scott.werner@gmail.com>
2021-03-16 14:10:31 -0500

    Created Index View; added View to grab and display the list of created Superheroes. Added/Created 3 Superheros to the Db.

--- SuperheroCreator_proj/Controllers/SuperheroesController.cs
+++ SuperheroCreator_proj/Controllers/SuperheroesController.cs
@@ -19,7 +19,7 @@ namespace SuperheroCreator_proj.Controllers
         // GET: SuperheroesController
         public ActionResult Index()
         {
-            return View();
+            return View(_context.Superheroes);
         }
 
         // GET: SuperheroesController/Details/5
